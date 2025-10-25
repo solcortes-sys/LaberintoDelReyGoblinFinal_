e6ef2d7a64df66dea35473e98161dcb2b6ea33c0
pabloernestomartinez-collab <pabloernestomartinez@upc.edu.ar>
2025-10-25 15:46:14 +0200

    elimino room4

--- Assets/Script/canvas/UImanagement.cs
+++ Assets/Script/canvas/UImanagement.cs
@@ -24,7 +24,22 @@ public class UImanagement : MonoBehaviour
     void Start()
     {
         Scene currentScene = SceneManager.GetActiveScene();        // Obtiene la escena activa
-        cartel.text = "Lives in " + currentScene.name;// Imprime el nombre de la escena en la consola
+        if (currentScene.name == "Room1")
+        {
+            cartel.text = "           Room: [ 1 ] > [   ] > [   ] > [   ]";// Imprime el nombre de la escena 1 en la consola
+        }
+        if (currentScene.name == "Room2")
+        {
+            cartel.text = "           Room: [   ] > [ 2 ] > [   ] > [   ]";// Imprime el nombre de la escena 2 en la consola
+        }
+        if (currentScene.name == "Room3")
+        {
+            cartel.text = "           Room: [   ] > [   ] > [ 3 ] > [   ]";// Imprime el nombre de la escena 3 en la consola
+        }
+        if (currentScene.name == "RoomFinal")
+        {
+            cartel.text = "           Room: [   ] > [   ] > [   ] > [END]";// Imprime el nombre de la escena 4 en la consola
+        }
         character = '/'; // El carácter que repito
         repeatCount = 100;
     }
